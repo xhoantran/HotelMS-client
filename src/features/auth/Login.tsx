3f93f38f2c6b6bd6d00e31883d21d94e706df479
Hoan Tran <xhoantran@gmail.com>
2023-08-10 18:37:40 -0400

    resize logo

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -73,7 +73,7 @@ const Login = () => {
     <Page title="Login - HotelMS">
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Logo className="mx-auto h-24 w-auto" />
+          <Logo className="mx-auto w-full" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
